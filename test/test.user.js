1559ffcf69846725a1983c12a6eff41257d6ba37
WayneLiang <lcw_wei@163.com>
2015-10-23 16:36:00 +0800

    增加token

--- test/test.user.js
+++ test/test.user.js
@@ -3,32 +3,38 @@ var testConfig  = require( './config' );
 var easemobSDK 	= require( '../index' );
 
 describe( 'User Test', function(){
-    before( function( done ){
-        // Init the SDK before testing.
-        easemobSDK.init(testConfig.org_name,testConfig.app_name,testConfig.client_id,testConfig.client_secret);
-        done();
+  before( function( done ){
+    // Init the SDK before testing.
+    easemobSDK.init(testConfig.org_name,testConfig.app_name,testConfig.client_id,testConfig.client_secret);
+    easemobSDK.get_token(testConfig.client_id,testConfig.client_secret,function(err,result){
+
     });
+    var result =  easemobSDK.get_token();
+    done();
+  });
 
-    after( function( done ) {
+  after( function( done ) {
+    done();
+  });
+
+  describe( 'Create user', function() {
+    it( 'Should return OK', function( done ) {
+      easemobSDK.user.create(testConfig.user.username,testConfig.user.password,function( err, res,body ){
+
+        should.not.exists( err );
+
+        console.log(res.statusCode);
+        res.statusCode.should.equal( 200 );
+
+        console.log(body.entities[0].username);
+        body.entities[0].username.should.equal(testConfig.user.username);
+
         done();
-    } );
 
-    describe( 'Create user', function() {
-        it( 'Should return OK', function( done ) {
-            easemobSDK.user.create(testConfig.user.username,testConfig.user.password,function( err, res,body ){
 
-                should.not.exists( err );
-
-                console.log(res.statusCode);
-                res.statusCode.should.equal( 200 );
-
-                console.log(body.entities[0].username);
-                body.entities[0].username.should.equal(testConfig.user.username);
-
-                done();
-            });
-
-        } );
+      });
 
     } );
+
+  } );
 });
