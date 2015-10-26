5edfe142547d612ec1df0cc9e9cd0be35240e209
WayneLiang <lcw_wei@163.com>
2015-10-26 13:59:34 +0800

    修改token request

--- test/config.js
+++ test/config.js
@@ -1,11 +1,18 @@
 module.exports = {
-    client_id 		: 'YXA6hIVhoHb_EeWcYR1pWLDUmQ',
-    client_secret 	: 'YXA6CkC7ljyuOkHSc9APL1bnmxXPVzA',
+  client_id 		: 'YXA6hIVhoHb_EeWcYR1pWLDUmQ',
+  client_secret 	: 'YXA6CkC7ljyuOkHSc9APL1bnmxXPVzA',
     org_name        : 'wayneliang',
     app_name        : 'test',
     user : {
-        username        : 'wayne',
-        password        : '123456'
+      username        : 'wayne',
+      password        : '123456'
     },
+    batch_user :[{
+      username        : 'waynetest1',
+      password        : '123456'
+    },{
+      username        : 'waynetest2',
+      password        : '123456'
+    }],
 
 }
--- test/test.user.js
+++ test/test.user.js
@@ -1,6 +1,7 @@
 var should 		= require( 'should' );
 var testConfig  = require( './config' );
 var easemobSDK 	= require( '../index' );
+var app_const       = require( '../lib/const');
 
 describe( 'User Test', function(){
   before( function( done ){
@@ -8,9 +9,11 @@ describe( 'User Test', function(){
     easemobSDK.init(testConfig.org_name,testConfig.app_name,testConfig.client_id,testConfig.client_secret);
     easemobSDK.get_token(function(err,result){
       if(err){
-//        console.log(err);
+        console.log(err);
+        console.log(result.statusCode);
       }else{
-        easemobSDK.token = result;
+        app_const.TOKEN = result;
+        console.log(app_const.TOKEN );
       }
     });
 
@@ -23,19 +26,58 @@ describe( 'User Test', function(){
 
   describe( 'Create user', function() {
     it( 'Should return OK', function( done ) {
-      console.log('-------------++++++++++++++++');
+
       easemobSDK.user.create(testConfig.user.username,testConfig.user.password,function( err, res,body ){
-        console.log('-------------');
+
         should.not.exists( err );
 
-        console.log(res.statusCode);
+
         res.statusCode.should.equal( 200 );
 
-//        console.log(body.entities[0].username);
+
         body.entities[0].username.should.equal(testConfig.user.username);
 
         done();
 
+
+      });
+
+      //done();
+    } );
+
+  } );
+
+  describe( 'Batch create user', function() {
+    it( 'Should return OK', function( done ) {
+      easemobSDK.user.create_batch(testConfig.batch_user ,function( err, res,body ){
+
+        should.not.exists( err );
+
+        res.statusCode.should.equal( 200 );
+
+        body.entities[0].username.should.equal(testConfig.batch_user[0].username);
+        body.entities[1].username.should.equal(testConfig.batch_user[1].username);
+
+        done();
+
+      });
+
+      //done();
+    } );
+
+  } );
+
+  describe( 'Get user', function() {
+    it( 'Should return OK', function( done ) {
+      easemobSDK.user.get(testConfig.user.username ,function( err, res,body ){
+
+        should.not.exists( err );
+
+        res.statusCode.should.equal( 200 );
+
+        body.entities[0].username.should.equal(testConfig.user.username);
+
+        done();
 
       });
 
