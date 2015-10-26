707e78b82e6a2c70a3bf5be27b5c777025d5efbf
liman <wanglimin1986@gmail.com>
2015-10-26 18:05:55 +0800

    add friend

---
+++ test/config_friend.js
@@ -0,0 +1,19 @@
+module.exports = {
+    client_id 		: 'YXA6hIVhoHb_EeWcYR1pWLDUmQ',
+    client_secret 	: 'YXA6CkC7ljyuOkHSc9APL1bnmxXPVzA',
+    org_name        : 'wayneliang',
+    app_name        : 'test',
+    user : {
+        username        : 'liman',
+        friend	        : 'limintest1'
+    },
+    add_user: {
+	username	: 'liman',
+	friend		:'limintest2'
+},
+    delete_user: {
+	username	:'liman',
+	friend          : 'limintest1'
+    },
+
+}
