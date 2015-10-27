82bf106dd331790b3122aa3959d2b781ed317eda
liman <wanglimin1986@gmail.com>
2015-10-27 18:35:51 +0800

    add group

--- test/config_friend.js
+++ test/config_friend.js
@@ -1,19 +1,9 @@
 module.exports = {
-    client_id 		: 'YXA6hIVhoHb_EeWcYR1pWLDUmQ',
-    client_secret 	: 'YXA6CkC7ljyuOkHSc9APL1bnmxXPVzA',
+    TOKEN               :'',
+    client_id           : 'YXA6-YTjsHxNEeWVnx-IcIrCoA',
+    client_secret       : 'YXA6momYwJOdTgHQeyTbkwV2KJiR2qg',
     org_name        : 'wayneliang',
-    app_name        : 'test',
-    user : {
-        username        : 'liman',
-        friend	        : 'limintest1'
-    },
-    add_user: {
-	username	: 'liman',
-	friend		:'limintest2'
-},
-    delete_user: {
-	username	:'liman',
-	friend          : 'limintest1'
-    },
+    app_name        : 'mytest',
 
 }
+
