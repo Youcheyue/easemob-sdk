ead12be2729f253a94767588bfd8631f7a32b844
Leo <zmliu0077@gmail.com>
2015-11-02 13:24:18 +0800

    fix bug

--- README.md
+++ README.md
@@ -17,6 +17,9 @@
 
 easemob nodejs sdk
 
+https://nodei.co/npm/easemob-sdk.png?downloads=true
+[![npm status](https://nodei.co/npm/easemob-sdk.svg?downloads=true&stars=true&downloadRank=true)](https://www.npmjs.com/package/easemob-sdk)
+
 
 ## Installation
 
--- test/group.test.js
+++ test/group.test.js
@@ -10,6 +10,7 @@ describe( 'Group test', function(){
     easemobSDK.init(testConfig.org_name,testConfig.app_name,testConfig.client_id,testConfig.client_secret);
     easemobSDK.get_token(function(err,result){
       if(!err){
+        token = result;
         done();
       }
     });
